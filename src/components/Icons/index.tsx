90804b4a6a5a5ffa8fe85df9ba6250a694439e03
Majid Alinejad <alinejad.mj@gmail.com>
2022-08-29 15:18:09 +0430

    font , icons  and tailwind config edited

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -8,3 +8,16 @@ export { ReactComponent as Envelope } from 'src/assets/svg/Envelope.svg';
 
 // Footer
 export { ReactComponent as Signal } from 'src/assets/svg/Signal.svg';
+
+// Sidebar
+export { ReactComponent as BasketIcon } from 'src/assets/svg/Basket.svg';
+export { ReactComponent as Envelope2Icon } from 'src/assets/svg/Envelope2.svg';
+export { ReactComponent as EyeFrameIcon } from 'src/assets/svg/EyeFrame.svg';
+export { ReactComponent as FileIcon } from 'src/assets/svg/File.svg';
+export { ReactComponent as File2Icon } from 'src/assets/svg/File2.svg';
+export { ReactComponent as GearIcon } from 'src/assets/svg/Gear.svg';
+export { ReactComponent as HomeIcon } from 'src/assets/svg/Home.svg';
+export { ReactComponent as MenuIcon } from 'src/assets/svg/Menu.svg';
+export { ReactComponent as MonitorIcon } from 'src/assets/svg/Monitor.svg';
+export { ReactComponent as QMarkIcon } from 'src/assets/svg/QMark.svg';
+export { ReactComponent as QuitIcon } from 'src/assets/svg/Quit.svg';
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,112 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  darkMode: 'class',
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ['./src/**/*.{js,jsx,ts,tsx}'],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                L: {
+                    basics: {
+                        10: '#F5F5F5',
+                        20: '#171840',
+                    },
+                    primary: {
+                        10: '#254A6F',
+                        20: '#2E4256',
+                    },
+                    menu: {
+                        10: '#C5D6E7',
+                        20: '#166BF3',
+                    },
+                    gray: {
+                        5: '#DEDCE4',
+                        10: '#F9FAFB',
+                        20: '#F0F4F9',
+                        30: '#EDF2F8',
+                        40: '#D4E0ED',
+                        50: '#ADB3C2',
+                        60: '#969FB0',
+                        70: '#7E89A0',
+                        80: '#454C5E',
+                    },
+                    yellow: {
+                        10: '#FFB11A',
+                        20: '#F4A203',
+                    },
+                    red: {
+                        10: '#F7CFCF',
+                        20: '#E04040',
+                    },
+                    green: {
+                        10: '#A7DFC1',
+                        20: '#24AE64',
+                    },
+                    blue: {
+                        10: '#939CAD',
+                        20: '#254A6F',
+                    },
+                    black: {
+                        10: '#333333',
+                        20: '#130F26',
+                    },
+                },
+                D: {
+                    basics: {
+                        10: '#1B1C22',
+                        20: '#000000',
+                    },
+                    primary: {
+                        10: '#254A6F',
+                        20: '#2E4256',
+                    },
+                    menu: {
+                        10: '#C5D6E7',
+                    },
+                    gray: {
+                        10: '#F9FAFB',
+                        20: '#F0F4F9',
+                        30: '#EDF2F8',
+                        40: '#D4E0ED',
+                        50: '#ADB3C2',
+                        60: '#969FB0',
+                        70: '#7E89A0',
+                        80: '#454C5E',
+                    },
+                    yellow: {
+                        10: '#FFB11A',
+                        20: '#F4A203',
+                    },
+                    red: {
+                        10: '#F7CFCF',
+                        20: '#E04040',
+                    },
+                    green: {
+                        10: '#A7DFC1',
+                        20: '#24AE64',
+                    },
+                    blue: {
+                        10: '#939CAD',
+                        20: '#254A6F',
+                    },
+                    black: {
+                        10: '#333333',
+                        20: '#130F26',
+                    },
+                },
+            },
+            fontSize: {
+                0.5: '0.3125rem',
+                0.6: '0.375rem',
+                0.7: '0.4375rem',
+                0.8: '0.5rem',
+                0.9: '0.5625rem',
+                1: '0.625rem',
+                1.1: '0.6875rem',
+                1.2: '0.75rem',
+                1.3: '0.8125rem',
+                1.4: '0.875rem',
+            },
+        },
+    },
+    plugins: [],
+};
