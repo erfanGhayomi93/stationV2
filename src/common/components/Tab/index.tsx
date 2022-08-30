82ec89dde818aaf70ad7182ba6e3b56f1483d1a6
Majid Alinejad <alinejad.mj@gmail.com>
2022-08-30 16:33:45 +0430

    tab configed

---
+++ src/common/components/Tab/index.tsx
@@ -0,0 +1,76 @@
+import { Children, cloneElement, FC, ReactElement } from 'react';
+import { Tab as HeadlessTab } from '@headlessui/react';
+import clsx from 'clsx';
+interface ITabType {
+    leftNode?: JSX.Element;
+    onChange: (index: string) => void;
+    selectedIndex?: string;
+    items: Item[];
+}
+interface Item {
+    key: string;
+    title: JSX.Element | string;
+    content: JSX.Element;
+}
+interface ITabButtonType {
+    children: JSX.Element;
+    key: string;
+}
+
+const Tab: FC<ITabType> = ({ leftNode, onChange, selectedIndex, items }) => {
+    return (
+        <HeadlessTab.Group
+            onChange={(index) => onChange(items[index].key)}
+            selectedIndex={items && items.findIndex((item) => item.key === selectedIndex)}
+        >
+            <HeadlessTab.List className=" bg-[#BFDBF7]">
+                <div className="flex justify-between items-center">
+                    <div>
+                        {items ? (
+                            items.map((item) => (
+                                <TabButton key="1">
+                                    <>{item.title}</>
+                                </TabButton>
+                            ))
+                        ) : (
+                            <></>
+                        )}
+                    </div>
+                    {leftNode}
+                </div>
+            </HeadlessTab.List>
+            <HeadlessTab.Panels>
+                {items ? (
+                    items.map((item) => (
+                        <HeadlessTab.Panel key={item.key}>
+                            <>{item.content}</>
+                        </HeadlessTab.Panel>
+                    ))
+                ) : (
+                    <></>
+                )}
+            </HeadlessTab.Panels>
+        </HeadlessTab.Group>
+    );
+};
+
+const TabButton: FC<ITabButtonType> = ({ children }) => {
+    return (
+        <HeadlessTab
+            className={({ selected }) =>
+                clsx(
+                    'border-t-2 py-2 px-5 border-solid outline-none',
+                    selected ? ' text-[#135CA4] border-[#135CA4] bg-white font-semibold' : ' border-transparent text-[#333333]',
+                )
+            }
+        >
+            {({ selected }) => cloneElement(children, { selected })}
+        </HeadlessTab>
+    );
+};
+
+TabButton.displayName = 'TabButton';
+HeadlessTab.Panel.displayName = 'TabPanel';
+
+// export const Tab = { Wrapper: TabWrapper, Button: TabButton, Panel: HeadlessTab.Panel };
+export default Tab;
