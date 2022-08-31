d6668056817111448c49efe89f196a2afad2670c
Khalil Shakhi <75076708+shakhi95@users.noreply.github.com>
2022-08-31 17:03:39 +0430

    Input Component Added

--- src/common/components/Input/index.tsx
+++ src/common/components/Input/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
-const Input = () => {
+interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
+    addonBefore?: JSX.Element;
+    addonAfter?: JSX.Element;
+}
+
+const Input = ({ disabled, addonAfter, addonBefore, ...rest }: Props) => {
     //
     return (
-        <div className="bg-white rounded-sm px-2">
-            <input type="text" className="w-full h-full px-1 py-2 outline-none" />
+        <div className={`flex items-center w-100 rounded-sm px-2 ${disabled ? 'bg-gray-100' : 'bg-white'}`}>
+            <div>{addonBefore ? addonBefore : null}</div>
+            <div className="grow">
+                <input disabled={disabled} type="text" className="w-full h-full px-2 py-2 outline-none" {...rest} />
+            </div>
+            <div>{addonAfter ? addonAfter : null}</div>
         </div>
     );
 };
--- src/common/widgets/SymbolDetail/SymbolSearch.tsx
+++ src/common/widgets/SymbolDetail/SymbolSearch.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Input from 'src/common/components/Input';
+import { Search } from 'src/common/icons';
 
 const SymbolSearch = () => {
     //
     return (
         <div>
-            <Input />
+            <Input placeholder="جستجوی نماد" addonBefore={<Search className="text-gray-400" />} />
         </div>
     );
 };
