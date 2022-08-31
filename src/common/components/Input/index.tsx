ec76d3fd4a3b99ad909806ff4b4f55594d57587c
Khalil Shakhi <75076708+shakhi95@users.noreply.github.com>
2022-08-31 16:37:52 +0430

    symbol detail init

---
+++ src/common/components/Input/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Input = () => {
+    //
+    return (
+        <div className="bg-white rounded-sm px-2">
+            <input type="text" className="w-full h-full px-1 py-2 outline-none" />
+        </div>
+    );
+};
+
+export default Input;
---
+++ src/common/widgets/SymbolDetail/SymbolSearch.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Input from 'src/common/components/Input';
+
+const SymbolSearch = () => {
+    //
+    return (
+        <div>
+            <Input />
+        </div>
+    );
+};
+
+export default SymbolSearch;
