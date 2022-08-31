02d600d2ebf9c7b42c755a452e6864b3be517d1b
Khalil Shakhi <75076708+shakhi95@users.noreply.github.com>
2022-08-31 15:46:56 +0430

    useMemo / scroll / TabsList

--- src/common/widgets/Reports/index.tsx
+++ src/common/widgets/Reports/index.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import Tab from 'src/common/components/Tab';
+import React, { useMemo, useState } from 'react';
+import TabsList from 'src/common/components/TabsList';
 import Drafts from './tabs/Drafts';
 import FailedOrders from './tabs/FailedOrders';
 import GroupOrders from './tabs/GroupOrders';
@@ -11,44 +11,43 @@ const Reports = () => {
     //
     const [activeTab, setActiveTab] = useState('Requests');
 
-    const items = [
-        {
-            key: 'Requests',
-            title: 'درخواست‌ها',
-            content: <Requests />,
-        },
-        {
-            key: 'OpenOrders',
-            title: 'سفارشات باز',
-            content: <OpenOrders />,
-        },
-        {
-            key: 'DoneOrders',
-            title: 'سفارشات انجام شده',
-            content: <DoneOrders />,
-        },
-        {
-            key: 'FailedOrders',
-            title: 'سفارشات خطا دار',
-            content: <FailedOrders />,
-        },
-        {
-            key: 'Drafts',
-            title: 'پیش نویس ها',
-            content: <Drafts />,
-        },
-        {
-            key: 'GroupOrders',
-            title: 'سفارشات گروهی',
-            content: <GroupOrders />,
-        },
-    ];
-
-    return (
-        <div className="w-full h-full bg-red-200 flex flex-col">
-            <Tab onChange={(idx) => setActiveTab(idx)} selectedIndex={activeTab} items={items} />
-        </div>
+    const items = useMemo(
+        () => [
+            {
+                key: 'Requests',
+                title: 'درخواست‌ها',
+                content: <Requests />,
+            },
+            {
+                key: 'OpenOrders',
+                title: 'سفارشات باز',
+                content: <OpenOrders />,
+            },
+            {
+                key: 'DoneOrders',
+                title: 'سفارشات انجام شده',
+                content: <DoneOrders />,
+            },
+            {
+                key: 'FailedOrders',
+                title: 'سفارشات خطا دار',
+                content: <FailedOrders />,
+            },
+            {
+                key: 'Drafts',
+                title: 'پیش نویس ها',
+                content: <Drafts />,
+            },
+            {
+                key: 'GroupOrders',
+                title: 'سفارشات گروهی',
+                content: <GroupOrders />,
+            },
+        ],
+        [],
     );
+
+    return <TabsList onChange={(idx) => setActiveTab(idx)} selectedIndex={activeTab} items={items} />;
 };
 
 export default Reports;
