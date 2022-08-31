9fb559b05427cc1f3cadd9be92cd718bcc0a9fe5
Khalil Shakhi <75076708+shakhi95@users.noreply.github.com>
2022-08-31 09:13:00 +0430

    some tabs added

--- src/common/widgets/PortfolioWatchlist/index.tsx
+++ src/common/widgets/PortfolioWatchlist/index.tsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Tab from 'src/common/components/Tab';
+import { GearIcon } from 'src/common/icons';
+import Portfolio from './tabs/Portfolio';
+import Watchlist from './tabs/Watchlist';
 
 const PortfolioWatchlist = () => {
     //
-    return <div className="w-full h-full bg-red-200">PortfolioWatchlist</div>;
+    const [activeTab, setActiveTab] = useState('Portfolio');
+
+    const items = [
+        {
+            key: 'Watchlist',
+            title: <>دیده‌بان</>,
+            content: <Watchlist />,
+        },
+        {
+            key: 'Portfolio',
+            title: <>پرتفوی مشتری</>,
+            content: <Portfolio />,
+        },
+    ];
+
+    return (
+        <div className="w-full h-full bg-red-200">
+            <Tab onChange={(idx) => setActiveTab(idx)} selectedIndex={activeTab} items={items} />
+        </div>
+    );
 };
 
 export default PortfolioWatchlist;
--- src/common/widgets/Reports/index.tsx
+++ src/common/widgets/Reports/index.tsx
@@ -1,8 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Tab from 'src/common/components/Tab';
+import Drafts from './tabs/Drafts';
+import FailedOrders from './tabs/FailedOrders';
+import GroupOrders from './tabs/GroupOrders';
+import OpenOrders from './tabs/OpenOrders';
+import Requests from './tabs/Requests';
+import DoneOrders from './tabs/DoneOrders';
 
 const Reports = () => {
     //
-    return <div className="w-full h-full bg-red-200">Reports</div>;
+    const [activeTab, setActiveTab] = useState('Requests');
+
+    const items = [
+        {
+            key: 'Requests',
+            title: 'درخواست‌ها',
+            content: <Requests />,
+        },
+        {
+            key: 'OpenOrders',
+            title: 'سفارشات باز',
+            content: <OpenOrders />,
+        },
+        {
+            key: 'DoneOrders',
+            title: 'سفارشات انجام شده',
+            content: <DoneOrders />,
+        },
+        {
+            key: 'FailedOrders',
+            title: 'سفارشات خطا دار',
+            content: <FailedOrders />,
+        },
+        {
+            key: 'Drafts',
+            title: 'پیش نویس ها',
+            content: <Drafts />,
+        },
+        {
+            key: 'GroupOrders',
+            title: 'سفارشات گروهی',
+            content: <GroupOrders />,
+        },
+    ];
+
+    return (
+        <div className="w-full h-full bg-red-200">
+            <Tab onChange={(idx) => setActiveTab(idx)} selectedIndex={activeTab} items={items} />
+        </div>
+    );
 };
 
 export default Reports;
