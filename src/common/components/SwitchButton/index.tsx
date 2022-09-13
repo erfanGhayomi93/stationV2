2bdfd5de741aa93ce297f784547cb9cdf72bff3f
m.alinejad <alinejad.ui@gmail.com>
2022-09-13 17:02:32 +0430

    design buy/sell added

---
+++ src/common/components/SwitchButton/index.tsx
@@ -0,0 +1,29 @@
+import { Switch } from '@headlessui/react';
+import { createContext, FC, useContext, useState } from 'react';
+import { SwitchButton } from './SwitchButton';
+
+interface ISwitchContextType {
+    checked: boolean;
+}
+
+export const SwitchContext = createContext<ISwitchContextType>({ checked: false });
+export const useSwitchValue = () => useContext(SwitchContext);
+interface ISwitcherType {
+    children?: JSX.Element;
+    defaultValue?: boolean;
+}
+
+const Switcher: FC<ISwitcherType> = ({ children = <SwitchButton />, defaultValue = false }) => {
+    const [checked, setChecked] = useState<boolean>(defaultValue);
+    return (
+        <SwitchContext.Provider value={{ checked }}>
+            <div dir="ltr">
+                <Switch onChange={setChecked} checked={checked}>
+                    {children}
+                </Switch>
+            </div>
+        </SwitchContext.Provider>
+    );
+};
+
+export default Switcher;
