88fb031b82dfd40aa85981b7a1d8c20f0b3f12bf
m.alinejad <alinejad.ui@gmail.com>
2022-10-08 16:58:48 +0330

    toast handled

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,7 @@ import { store } from 'src/redux/store';
 // Providers
 import TranslatorProvider from 'src/app/i18n';
 import QueryClientProvider from 'src/app/queryClient';
+import { ToastContainer } from 'react-toastify';
 
 // Components
 import App from 'src/app/App';
@@ -15,6 +16,7 @@ import App from 'src/app/App';
 // Styles
 import 'src/common/components/LinearRangeChart/build.css';
 import 'src/assets/scss/main.scss';
+import 'react-toastify/dist/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
@@ -24,6 +26,16 @@ root.render(
             <TranslatorProvider>
                 <QueryClientProvider>
                     <App />
+                    <ToastContainer
+                        position="bottom-left"
+                        autoClose={5000}
+                        hideProgressBar={true}
+                        newestOnTop={false}
+                        closeOnClick
+                        rtl={false}
+                        draggable
+                        pauseOnHover
+                    />
                 </QueryClientProvider>
             </TranslatorProvider>
         </BrowserRouter>
