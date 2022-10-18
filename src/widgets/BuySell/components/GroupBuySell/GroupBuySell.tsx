1313b42bb4a9ab6c890ede39ac063923f56322e6
m.alinejad <alinejad.ui@gmail.com>
2022-10-18 15:05:54 +0330

    add customer to buysell widget

--- src/widgets/BuySell/components/GroupBuySell/GroupBuySell.tsx
+++ src/widgets/BuySell/components/GroupBuySell/GroupBuySell.tsx
@@ -1,6 +1,4 @@
-import SimpleDatepicker from 'src/common/components/Datepicker/SimpleDatepicker';
-import Input from 'src/common/components/Input';
-import { useAppValues } from 'src/redux/hooks';
+import BuySellCustomer from '../BuySellCustomer';
 import BuySellDetail from '../Detail/BuySellDetail';
 import BuySellPrice from '../Price';
 import BuySellQuantity from '../Quantity';
@@ -12,19 +10,18 @@ import BuySellSymbol from '../Symbol';
 import BuySellValidity from '../Validity';
 
 const GroupBuySell = () => {
-    const {
-        option: { selectedCustomers },
-    } = useAppValues();
-
     return (
         <div className="flex flex-col text-1.2 p-2 h-full relative justify-between">
             <div className="flex flex-col py-1 grow justify-around">
                 <div className="flex w-full gap-4 pr-2">
                     <label className="w-full flex items-center justify-center ">
                         <span className="w-[64px] whitespace-nowrap ">مشتری</span>
-                        <div className="w-full border-L-gray-350 dark:border-D-gray-350 border overflow-hidden rounded-md">
-                            <Input value={selectedCustomers.map((c) => c.customerTitle)} onChange={() => {}} />
+                        <div className="w-full">
+                            <BuySellCustomer />
                         </div>
+                        {/* <div className="w-full border-L-gray-350 dark:border-D-gray-350 border overflow-hidden rounded-md">
+                            <Input value={selectedCustomers.map((c) => c.customerTitle)} onChange={() => {}} />
+                        </div> */}
                     </label>
                 </div>
 
