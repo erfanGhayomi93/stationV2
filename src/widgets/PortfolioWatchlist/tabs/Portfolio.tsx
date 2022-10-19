8fc5e43e3d690f82c69935df552537870d9bbce4
erfanGhayomi93 <erfuunghayomi@gmail.com>
2022-10-19 13:52:27 +0330

    set empty table in portfolio

--- src/widgets/PortfolioWatchlist/tabs/Portfolio.tsx
+++ src/widgets/PortfolioWatchlist/tabs/Portfolio.tsx
@@ -1,8 +1,38 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import AGTable, { ColDefType } from 'src/common/components/AGTable';
+
+type RequestData = {
+    customerTitle: string;
+    symbolISIN: string;
+    orderSide: string;
+    quantity: number;
+    sumExecuted: number;
+    price: number;
+    valuePosition: number;
+    // creditRequest: Boolean;
+};
 
 const Portfolio = () => {
-    //
-    return <div>Portfolio</div>;
+    const columns = useMemo(
+        (): ColDefType<RequestData>[] => [
+            { headerName: 'نماد', field: 'customerTitle' },
+            { headerName: 'تعداد', field: 'quantity', type: 'sepratedNumber' },
+            { headerName: 'قیمت سر‌به‌سر', field: 'price', type: 'sepratedNumber' },
+            { headerName: 'ارزش دارایی', field: 'valuePosition', type: 'sepratedNumber' },
+            { headerName: 'ایستگاه', field: 'symbolISIN' },
+            { headerName: 'درصد سود و زیان', field: 'orderSide' },
+            { headerName: 'مبلغ سود و زیان', field: 'orderSide2' },
+            { headerName: 'سود مجمع', field: 'orderSide3' },
+          
+        ],
+        [],
+    );
+
+    return (
+        <div className="w-full h-full p-3">
+            <AGTable rowData={[]} columnDefs={columns} />
+        </div>
+    );
 };
 
 export default Portfolio;
