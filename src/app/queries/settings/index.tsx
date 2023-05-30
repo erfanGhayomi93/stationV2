7dfcbcbc61929239954c507ca38591c112ce5e54
erfanGhayomi93 <erfuunghayomi@gmail.com>
2023-05-30 13:58:31 +0330

    changed to common.ramandtech.com

--- src/app/queries/settings/index.tsx
+++ src/app/queries/settings/index.tsx
@@ -5,7 +5,7 @@ import { setAppState } from 'src/redux/slices/global';
 
 export const getGlobalSettings = async () => {
     // const { data } = await AXIOS.get<ISettingsType[]>('http://192.168.40.8:12000/Setting/v1/GTGetSettings');
-    const { data } = await AXIOS.get<ISettingsType[]>('https://common-stage.ramandtech.com/Setting/v1/GTGetSettings');
+    const { data } = await AXIOS.get<ISettingsType[]>('https://common.ramandtech.com/Setting/v1/GTGetSettings');
 
     // console.log({ data });
     return data || [];
