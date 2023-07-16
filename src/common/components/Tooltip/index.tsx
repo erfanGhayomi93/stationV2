ca504465cb173b47f3c9a508138f26e4d5a5f939
Javad basiri <javadbasiri.js@gmail.com>
2023-07-16 11:35:29 +0330

    update Tooltip component

--- src/common/components/Tooltip/index.tsx
+++ src/common/components/Tooltip/index.tsx
@@ -1,15 +1,30 @@
 import { FC } from 'react';
 
 type TOoltip = {
-    children : string | JSX.Element ;
-    title : string | JSX.Element ;
-}
+    children: string | JSX.Element;
+    title: string | JSX.Element;
+    position?: 'right' | 'left' | 'top' | 'bottom';
+};
 
-const Tooltip: FC<TOoltip> = ({children , title}) => {
+const Tooltip: FC<TOoltip> = ({ children, title, position = 'left' }) => {
     return (
-        <div className='group relative'>
+        <div className="group relative">
             {children}
-            <span className="hidden group-hover:inline-block absolute whitespace-nowrap right-[60px] px-2 py-1 bg-gray-700 rounded-lg text-center text-white text-sm z-50 top-2  before:content-[''] before:absolute before:top-1/2  before:-right-5 before:-translate-y-1/2 before:border-8 before:border-y-transparent before:border-r-transparent before:border-gray-700">
+            <span
+                style={{
+                    [position]: '-10px',
+                }}
+                className={`opacity-0 group-hover:opacity-100 duration-200 pointer-events-none absolute whitespace-nowrap px-2 py-1 bg-gray-700 rounded-lg text-center text-white text-sm z-50
+                ${
+                    position === 'bottom'
+                        ? 'translate-y-full right-1/2 translate-x-1/2'
+                        : position === 'top'
+                        ? '-translate-y-full right-1/2 translate-x-1/2'
+                        : position === 'right'
+                        ? 'translate-x-full -translate-y-1/2 top-1/2'
+                        : '-translate-x-full -translate-y-1/2 top-1/2'
+                }`}
+            >
                 {title}
             </span>
         </div>
