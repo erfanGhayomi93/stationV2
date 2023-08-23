bd3c3bbcd3914cac21944d7f0bee0b8b3e3b3b93
Javad basiri <javadbasiri.js@gmail.com>
2023-08-23 16:41:05 +0330

    best5row pushengine

--- src/widgets/SymbolDetail/SymbolData/tabs/OrderBook/components/Best5Row/index.tsx
+++ src/widgets/SymbolDetail/SymbolData/tabs/OrderBook/components/Best5Row/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useQueryClient } from '@tanstack/react-query';
 import { useAppValues } from 'src/redux/hooks';
 import HalfRow from '../HalfRow';
+import { pushEngine } from 'src/api/pushEngine';
 
 const Best5Row = () => {
     //
@@ -35,6 +36,57 @@ const Best5Row = () => {
     useEffect(() => {
         setTotalVolume();
     }, [orders]);
+
+    useEffect(() => {
+        if (selectedSymbol) {
+            pushEngine.subscribe({
+                id: 'SymbolBest5Orders',
+                mode: 'MERGE',
+                isSnapShot: 'yes',
+                adapterName: 'RamandRLCDData',
+                items: [selectedSymbol],
+                fields: [
+                    'numberOfOrdersAtBestBuy_1',
+                    'bestBuyLimitQuantity_1',
+                    'bestBuyLimitPrice_1',
+                    'numberOfOrdersAtBestBuy_2',
+                    'bestBuyLimitQuantity_2',
+                    'bestBuyLimitPrice_2',
+                    'numberOfOrdersAtBestBuy_3',
+                    'bestBuyLimitQuantity_3',
+                    'bestBuyLimitPrice_3',
+                    'numberOfOrdersAtBestBuy_4',
+                    'bestBuyLimitQuantity_4',
+                    'bestBuyLimitPrice_4',
+                    'numberOfOrdersAtBestBuy_5',
+                    'bestBuyLimitQuantity_5',
+                    'bestBuyLimitPrice_5',
+                    'numberOfOrdersAtBestSell_1',
+                    'bestSellLimitQuantity_1',
+                    'bestSellLimitPrice_1',
+                    'numberOfOrdersAtBestSell_2',
+                    'bestSellLimitQuantity_2',
+                    'bestSellLimitPrice_2',
+                    'numberOfOrdersAtBestSell_3',
+                    'bestSellLimitQuantity_3',
+                    'bestSellLimitPrice_3',
+                    'numberOfOrdersAtBestSell_4',
+                    'bestSellLimitQuantity_4',
+                    'bestSellLimitPrice_4',
+                    'numberOfOrdersAtBestSell_5',
+                    'bestSellLimitQuantity_5',
+                    'bestSellLimitPrice_5',
+                ],
+                onFieldsUpdate: ({ changedFields }) => {
+                    setOrders((pre) => ({ ...pre, ...changedFields } as OrdersData));
+                },
+            });
+        }
+
+        return () => {
+            pushEngine.unSubscribe('SymbolBest5Orders');
+        };
+    }, [selectedSymbol]);
 
     return (
         <div className="w-full">
