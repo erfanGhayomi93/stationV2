ee34e1f48a8e5f731e63d0345af4bcfa0353b645
Javad basiri <javadbasiri.js@gmail.com>
2023-10-08 10:51:29 +0330

    input issue

--- src/widgets/CustomerSearch/components/SearchInput/index.tsx
+++ src/widgets/CustomerSearch/components/SearchInput/index.tsx
@@ -2,7 +2,7 @@ import Input from 'src/common/components/Input';
 import { Search } from 'src/common/icons';
 import { useCustomerSearchState } from '../../context/CustomerSearchContext';
 type ISearchInputType = {
-    placeholder: string
+    placeholder: string;
 };
 
 const SearchInput = ({ placeholder }: ISearchInputType) => {
@@ -11,13 +11,12 @@ const SearchInput = ({ placeholder }: ISearchInputType) => {
         setState((prev) => ({ ...prev, params: { ...prev.params, term: input }, isSelectedActive: false }));
     };
     return (
-        <div className="border-L-gray-400 dark:border-D-gray-400 border overflow-hidden rounded-md">
+        <div>
             <Input
-                containerClassName="flex items-center w-[21.5rem] px-2 text-1.2 rounded-sm duration-250 dark:focus-within:border-D-info-100 focus-within:border-L-info-100"
                 placeholder={placeholder}
                 addonBefore={<Search className="text-gray-400" />}
                 onChange={(e) => setParams(e.target.value)}
-                value={state.params.term ?? ""}
+                value={state.params.term ?? ''}
             />
         </div>
     );
