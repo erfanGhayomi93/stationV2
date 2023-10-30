881a48513e420bcdf6e506ee753964cbad2eca7c
Javad basiri <javadbasiri.js@gmail.com>
2023-10-30 14:37:55 +0330

    alarm setting

--- src/pages/Setting/tabs/SystemSetting/AlarmSetting.tsx
+++ src/pages/Setting/tabs/SystemSetting/AlarmSetting.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import Select from 'src/common/components/Select';
 
 const AlarmSetting = () => {
-  return (
-    <div>AlarmSetting</div>
-  )
-}
+    //
+    const { t } = useTranslation();
 
-export default AlarmSetting
+    return (
+        <div className="h-full text-D-basic flex flex-col gap-4 text-sm dark:text-L-basic">
+            <div className="font-medium">{t('setting.alarms')}</div>
+            <div className="flex justify-between items-center">
+                <span>{t('setting.alarmSendingType')}</span>
+                <div className="w-1/3">
+                    <Select onChange={() => {}} value={true} options={[{ label: 'درون برنامه ای', value: true }]} />
+                </div>
+            </div>
+            <div className="border-b border-L-gray-300 dark:border-D-gray-300"></div>
+            <div className="flex justify-between items-center">
+                <span>{t('setting.alarmDisplayPosition')}</span>
+                <div className="w-1/3">
+                    <Select onChange={() => {}} value={true} options={[{ label: 'پایین - چپ', value: true }]} />
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AlarmSetting;
