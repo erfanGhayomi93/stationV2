33a16d0800fdb58e9847cab127dde7d48fe8d601
erfanGhayomi93 <erfuunghayomi@gmail.com>
2023-11-18 11:33:37 +0330

    fixed bug

--- src/common/hooks/useApiRoutes/useApiRoutes.tsx
+++ src/common/hooks/useApiRoutes/useApiRoutes.tsx
@@ -4,8 +4,8 @@ import { queryClient } from 'src/app/queryClient';
 
 export const useApiPath = () => {
     const [apiRoutes, setApiRoutes] = useState<any | undefined>();
-    // const { data } = useGlobalSettings();
-    const { data } = useGlobalSettingsPreprd();
+    const { data } = useGlobalSettings();
+    // const { data } = useGlobalSettingsPreprd();
 
     useEffect(() => {
         const routes = Apis();
