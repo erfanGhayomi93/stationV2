aca1374ea74b40a76f679d44e3464dafe5fe8b14
Erfan Ghayomi <e.ghayomi@ramandtech.com>
2023-11-26 10:01:19 +0000

    Update App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,42 +1,41 @@
 import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Route, Routes, createBrowserRouter, useNavigate } from 'react-router-dom';
-
-// import { transferFunctions } from 'src/api/axiosInstance';
-// import { AUTHORIZED_ROUTES, UN_AUTHORIZED_ROUTES } from 'src/app/routes/appRoutes';
 import AppRoutes from 'src/app/routes/appRoutes';
 import { fetchUser } from 'src/handlers/boot';
-
-import AppLayout from 'src/app/Layout';
 import RouteWrapper from 'src/common/components/RouteWrapper';
 import useLocalStorage from 'src/common/hooks/useLocalStorage';
 import CrashPage from 'src/pages/PageCrash';
 import { useAppDispatch, useAppSelector } from 'src/redux/hooks';
 import { setSelectedSymbol } from 'src/redux/slices/option';
 import { getAppState } from 'src/redux/slices/global';
-// import ipcMain from 'src/common/classes/IpcMain';
+import Loading from 'src/common/components/Loading/Loading';
+import Cookies from 'js-cookie';
+import { tokenCookieName, unAuthorized } from 'src/api/axiosInstance';
 
 const App = () => {
     const appState = useAppSelector(getAppState);
     const appDispatch = useAppDispatch();
     const [localSymbolISIN] = useLocalStorage<string>('symbolISIN', 'IRO1ATIR0001');
-
+    const hasCookie = Cookies.get(tokenCookieName)
     const {
         ready: isTranslationResourceReady,
         i18n: { resolvedLanguage },
     } = useTranslation();
 
-    //     useEffect(() => {
-    //         // transferFunctions(navigate, appDispatch);
-    //     }, []);
-    //
+
+
     useEffect(() => {
-        appState === 'Loading' && fetchUser(appDispatch);
-        appState === 'LoggedIn' && localSymbolISIN && appDispatch(setSelectedSymbol(localSymbolISIN));
+        // appState === 'Loading' && fetchUser(appDispatch);
+        // appState === 'LoggedIn' && localSymbolISIN && appDispatch(setSelectedSymbol(localSymbolISIN));
+        if (appState === "LoggedIn" || appState === "Loading") {
+            appDispatch(setSelectedSymbol(localSymbolISIN))
+            fetchUser(appDispatch);
+        }
     }, [appState]);
-    //
-   // if (appState === 'Booting' || appState === 'Loading' || !isTranslationResourceReady) return <>AppIsLoading...</>;
-    if (appState === 'Crashed' || resolvedLanguage !== 'fa') return <CrashPage />;
+
+    // if (!hasCookie) unAuthorized()
+    if (appState === 'Booting' || appState === 'Loading' || !isTranslationResourceReady) return <Loading />;
+    // if (appState === 'Crashed' || resolvedLanguage !== 'fa') return <CrashPage />;
 
     return (
         <RouteWrapper>
@@ -46,36 +45,3 @@ const App = () => {
 };
 
 export default App;
-
-//  (
-// <Routes>
-//     {appState === 'LoggedIn' ? (
-//         <Route path="/" element={<AppLayout />}>
-//             {createBrowserRouter(AUTHORIZED_ROUTES).map(({ path, component: Component }) => (
-//                 <Route
-//                     key={path}
-//                     path={path}
-//                     element={
-//                         <RouteWrapper>
-//                             <Component />
-//                         </RouteWrapper>
-//                     }
-//                 />
-//             ))}
-//         </Route>
-//     ) : null}
-//     {appState === 'LoggedOut'
-//               <Route
-//                   key={path}
-//                   path={path}
-//                   element={
-//                       <RouteWrapper>
-//                           <Component />
-//                       </RouteWrapper>
-//                   }
-//               />
-//           ))
-//         : null}
-// </Routes>
-
-// );
