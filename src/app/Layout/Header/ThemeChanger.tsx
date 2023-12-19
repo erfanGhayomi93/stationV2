5aa886de1558c2315ea1116469dfb73878fe9ba4
Alireza Sedighi <alireza.sedighi.job@gmail.com>
2023-12-19 11:37:46 +0330

    STAT-1300 created custom switch only for change theme

--- src/app/Layout/Header/ThemeChanger.tsx
+++ src/app/Layout/Header/ThemeChanger.tsx
@@ -1,8 +1,9 @@
 import { useAppDispatch, useAppSelector } from 'src/redux/hooks';
 import { getTheme, setAppTheme } from 'src/redux/slices/ui';
 import Switcher from 'src/common/components/SwitchButton';
-import { SwitchButton } from 'src/common/components/SwitchButton/SwitchButton';
+import { useSwitchValue } from 'src/common/components/SwitchButton';
 import { MoonIcon, SunIcon } from 'src/common/icons';
+import clsx from 'clsx';
 
 const ThemeChanger = () => {
     //
@@ -17,12 +18,25 @@ const ThemeChanger = () => {
             }}
             className="flex mx-2"
         >
-            <SwitchButton
-                className={theme === 'light' ? 'bg-yellow-300' : 'bg-blue-900'}
-                icon={theme === 'light' ? <SunIcon color="white" height={18} /> : <MoonIcon height={18} />}
-            />
+            <SwitchButton icon={theme === 'light' ? <SunIcon color="white" height={14} /> : <MoonIcon height={14} />} />
         </Switcher>
     );
 };
 
 export default ThemeChanger;
+
+const SwitchButton = ({ icon }: { icon?: any }) => {
+    const { checked } = useSwitchValue();
+    return (
+        <span className={clsx('relative outline-none flex h-5 w-10 items-center rounded-full', checked ? 'bg-[#2C529033]' : 'bg-[#FFB11A26]')}>
+            <span
+                className={clsx(
+                    'flex justify-center items-center h-6 w-6 transform rounded-full transition-transform',
+                    checked ? 'translate-x-[1.1rem] bg-[#2C5290]' : 'bg-[#F5A300]',
+                )}
+            >
+                {icon && icon}
+            </span>
+        </span>
+    );
+};
