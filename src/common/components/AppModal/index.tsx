0877f3a19e70b643c2d3c24327c388cd5ace3230
Alireza Sedighi <alireza.sedighi.job@gmail.com>
2024-01-17 11:20:21 +0330

    added appmodal component for order details

--- src/common/components/AppModal/index.tsx
+++ src/common/components/AppModal/index.tsx
@@ -6,21 +6,29 @@ type TProps = {
     width?: number;
     height?: number;
     isOpen: boolean;
-    setIsOpen: Dispatch<SetStateAction<boolean>>;
     className?: string;
     titleClassName?: string;
-    title?: string;
+    title?: string | React.ReactNode;
     children: React.ReactNode;
+    handleClose?: () => void;
 };
 
-const AppModal = ({ width = 500, height = 300, className = '', title = '', titleClassName = '', children, isOpen, setIsOpen }: TProps) => {
+const AppModal = ({
+    width = 500,
+    height = 300,
+    className = '',
+    title = '',
+    titleClassName = '',
+    children,
+    isOpen,
+    handleClose = () => {},
+}: TProps) => {
     //
-    const handleClose = () => setIsOpen(false);
 
     return (
         <Modal isOpen={isOpen} onClose={handleClose} className={`rounded ${className}`} style={{ height, width }}>
             <div className="w-full h-full bg-L-basic dark:bg-D-basic flex flex-col">
-                <div className={`flex justify-between items-center bg-L-primary-50 dark:bg-D-primary-200 px-6 h-12 ${titleClassName}`}>
+                <div className={`flex justify-between items-center bg-L-blue-200 dark:bg-D-primary-200 px-6 h-12 ${titleClassName}`}>
                     <span className="font-medium text-base text-white">{title}</span>
                     <button className="p-1" onClick={handleClose}>
                         <CloseIcon className="text-white" />
