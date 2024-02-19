c96447e5b0f08d146cef9cfc8cd46614b0efed22
erfanGhayomi93 <erfuunghayomi@gmail.com>
2024-02-19 11:07:50 +0330

    fixed bug

--- src/common/components/IpoBuyModal/components/Table/components/EditableColumn.tsx
+++ src/common/components/IpoBuyModal/components/Table/components/EditableColumn.tsx
@@ -1,5 +1,5 @@
 import { ICellRendererParams } from 'ag-grid-community';
-// import { EditIcon2 } from 'src/common/icons';
+import { EditIcon2 } from 'src/common/icons';
 import './style.scss';
 import Tippy from '@tippyjs/react';
 
@@ -12,7 +12,7 @@ const EditableColumn = ({ valueFormatted, tooltipContent }: IProps) => {
         <Tippy content={`ویرایش ${tooltipContent}`}>
             <div className="h-full w-full flex items-center justify-center gap-1 editableCol cursor-pointer">
                 {valueFormatted} 
-                {/* <EditIcon2 className="icon" height={16} width={16} /> */}
+                <EditIcon2 className="icon" height={16} width={16} />
             </div>
         </Tippy>
     );
