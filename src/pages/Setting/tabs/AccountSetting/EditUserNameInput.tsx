55f1dbda3e4048e7eb29f2fda52d130d4d8f3baa
erfanGhayomi93 <erfuunghayomi@gmail.com>
2024-02-20 15:58:09 +0330

    fixed bug

--- src/pages/Setting/tabs/AccountSetting/EditUserNameInput.tsx
+++ src/pages/Setting/tabs/AccountSetting/EditUserNameInput.tsx
@@ -1,11 +1,37 @@
 import React, { useRef, useEffect, useState } from 'react';
+import { useSetUsername } from 'src/app/queries/settings/PlatformSetting';
+import ConfirmationModal from 'src/common/components/ConfirmModal/ConfirmationModal';
 import Input from 'src/common/components/Input';
 import { Check, UnCheck } from 'src/common/icons';
+import { onErrorNotif, onSuccessNotif } from 'src/handlers/notification';
 
-const EditUserNameInput = ({ defaultValue, toggleEditing }: { defaultValue: string; toggleEditing: () => void }) => {
+const EditUserNameInput = ({ defaultValue, toggleEditing, userId }: { defaultValue: string; toggleEditing: () => void, userId?: number }) => {
     //
     const inputRef = useRef<HTMLInputElement>(null);
     const [value, setValue] = useState(defaultValue);
+    const { mutate } = useSetUsername();
+
+    const [isConfirmModalOpen, setIsConfirmModalOpen] = useState(false);
+
+    const handleConfirm = () => {
+        if (!userId) {
+            onErrorNotif({ title: 'مشکلی در دریافت اطلاعات نماد وجود دارد' });
+            return
+        }
+
+        mutate({ newUserName: value, userId }, {
+            onSuccess: (data) => {
+                if (data.succeeded) {
+                    onSuccessNotif({ title: "نام کاربری با موفقیت تغییر یافت" })
+                    toggleEditing()
+                }
+            }
+        })
+    }
+
+    const setUsername = () => {
+        setIsConfirmModalOpen(true)
+    }
 
     useEffect(() => {
         if (inputRef.current) {
@@ -14,25 +40,44 @@ const EditUserNameInput = ({ defaultValue, toggleEditing }: { defaultValue: stri
     }, [inputRef]);
 
     return (
-        <Input
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-            ref={inputRef}
-            style={{ direction: 'ltr' }}
-            addonBefore={
-                <div className="flex gap-1 items-center bg-L-basic dark:bg-D-basic">
-                    <span
-                        onClick={toggleEditing}
-                        className="cursor-pointer text-L-error-200 w-6 h-6 rounded bg-L-gray-200 dark:bg-D-gray-200 flex items-center justify-center"
-                    >
-                        <UnCheck />
-                    </span>
-                    <span className="cursor-pointer text-L-success-200 w-6 h-6 rounded bg-L-gray-200 dark:bg-D-gray-200 flex items-center justify-center">
-                        <Check />
-                    </span>
-                </div>
-            }
-        />
+        <>
+            <Input
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+                ref={inputRef}
+                style={{ direction: 'ltr' }}
+                addonBefore={
+                    <div className="flex gap-1 items-center bg-L-basic dark:bg-D-basic">
+                        <span
+                            onClick={toggleEditing}
+                            className="cursor-pointer text-L-error-200 w-6 h-6 rounded bg-L-gray-200 dark:bg-D-gray-200 flex items-center justify-center"
+                        >
+                            <UnCheck />
+                        </span>
+                        <span
+                            className="cursor-pointer text-L-success-200 w-6 h-6 rounded bg-L-gray-200 dark:bg-D-gray-200 flex items-center justify-center"
+                            onClick={setUsername}
+                        >
+                            <Check />
+                        </span>
+                    </div>
+                }
+            />
+
+            {isConfirmModalOpen && (
+                <ConfirmationModal
+                    isOpen={isConfirmModalOpen}
+                    title={"تغییر نام کاربری"}
+                    description={"بعد از تغییر نام کاربری به صفحه ورود انتقال پیدا می کنید"}
+                    onConfirm={handleConfirm}
+                    onCancel={() => {
+                        setIsConfirmModalOpen(false)
+                        toggleEditing()
+                    }}
+                    confirmBtnLabel="تایید"
+                />
+            )}
+        </>
     );
 };
 
