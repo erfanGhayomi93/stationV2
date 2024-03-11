d2d435520ead538e9b607bab78818e6186ac69fe
Alireza Sedighi <alireza.sedighi.job@gmail.com>
2024-03-11 10:16:01 +0330

    fixed HeaderSelect Bug

--- src/widgets/Reports/components/HeaderSelect.tsx
+++ src/widgets/Reports/components/HeaderSelect.tsx
@@ -9,7 +9,7 @@ interface IProps extends IHeaderParams {
 }
 
 const HeaderSelect = ({ displayName, onChange, value }: IProps) => {
-    const [isSelectAvtive, setIsSelectAvtive] = useState(false);
+    const [isSelectActive, setIsSelectActive] = useState(false);
     const inputRef = useRef<HTMLSelectElement>(null);
 
     const options = [
@@ -19,16 +19,16 @@ const HeaderSelect = ({ displayName, onChange, value }: IProps) => {
     ];
 
     useEffect(() => {
-        isSelectAvtive && inputRef.current?.focus();
-    }, [isSelectAvtive]);
+        isSelectActive && inputRef.current?.focus();
+    }, [isSelectActive]);
 
-    return isSelectAvtive ? (
+    return isSelectActive || value !== 'All' ? (
         <select
             ref={inputRef}
             value={value}
             className="w-full px-3 relative text-xs h-8 dark:focus-within:border-D-info-100 focus:outline-none focus-within:border-L-info-100 cursor-pointer focus-visible:ring-2 focus-visible:ring-transparent   bg-L-basic dark:bg-D-basic border-L-gray-400 dark:border-D-gray-400 border rounded-md"
             onChange={(e) => onChange && onChange(e.target.value)}
-            onBlur={() => setIsSelectAvtive(false)}
+            onBlur={() => setIsSelectActive(false)}
         >
             {options.map((i, x) => (
                 <option key={x} value={i.value}>
@@ -37,7 +37,7 @@ const HeaderSelect = ({ displayName, onChange, value }: IProps) => {
             ))}
         </select>
     ) : (
-        <div className="w-full flex items-center justify-center gap-3" onClick={() => setIsSelectAvtive(true)}>
+        <div className="w-full flex items-center justify-center gap-3" onClick={() => setIsSelectActive(true)}>
             <SearchIcon />
             <span>{displayName}</span>
         </div>
