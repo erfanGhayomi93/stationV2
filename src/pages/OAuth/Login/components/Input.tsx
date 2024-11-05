08ab5afe36d37dfd50101efafc834983e258b5da
f.janmohammadi <f.janmohammadi@ramandtech.com>
2024-11-05 16:15:28 +0330

    add swiper

--- src/pages/OAuth/Login/components/Input.tsx
+++ src/pages/OAuth/Login/components/Input.tsx
@@ -1,7 +1,7 @@
 import clsx from 'clsx';
 import React, { forwardRef } from 'react';
 
-interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
+interface Props extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'placeholder'> {
      addonBefore?: JSX.Element;
      addonAfter?: JSX.Element;
      containerClassName?: string;
@@ -9,14 +9,18 @@ interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
      label?: string;
      textError?: string;
      icon?: JSX.Element;
+     placeholder: string;
 }
 
 const Input = forwardRef<HTMLInputElement, Props>(
-     ({ disabled, addonAfter, addonBefore, containerClassName, inputClassName, icon, label, textError, ...rest }, ref) => {
+     (
+          { disabled, addonAfter, addonBefore, containerClassName, inputClassName, icon, label, textError, placeholder, ...rest },
+          ref
+     ) => {
           //
           return (
                // <div className={` ${containerClassName} ${disabled ? 'opacity-80' : 'bg-L-basic dark:bg-D-basic'}`}>
-               <>
+               <div className="group relative">
                     <div
                          className={clsx(
                               'duration-250 flex h-14 w-full items-center overflow-hidden rounded-2xl border border-input-default focus-within:border-input-primary',
@@ -33,8 +37,9 @@ const Input = forwardRef<HTMLInputElement, Props>(
                                    disabled={disabled}
                                    type="text"
                                    dir="ltr"
+                                   placeholder=""
                                    className={clsx(
-                                        'text-L-gray-700 dark:text-D-gray-700 h-full w-full bg-transparent pl-4 outline-none placeholder:text-right',
+                                        'text-L-gray-700 dark:text-D-gray-700 z-10 h-full w-full bg-transparent pl-4 outline-none placeholder:text-right',
                                         {
                                              [inputClassName as string]: !!inputClassName,
                                         }
@@ -51,7 +56,11 @@ const Input = forwardRef<HTMLInputElement, Props>(
                     >
                          {textError}
                     </p>
-               </>
+
+                    <div className="absolute right-12 top-1/2 z-0 -translate-y-1/2 text-content-placeholder transition-all duration-100 group-focus-within:-top-1 group-focus-within:right-4 group-focus-within:bg-back-surface group-focus-within:p-2 group-focus-within:text-xs group-focus-within:text-input-primary group-focus-within:after:content-['*']">
+                         <span>{placeholder}</span>
+                    </div>
+               </div>
           );
      }
 );
