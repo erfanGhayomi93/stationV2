39441f4ba1fcbc766a0e4b33a97715f5b5b828e3
erfanGhayomi93 <erfuunghayomi@gmail.com>
2024-11-12 16:46:49 +0330

    fixed bug

--- eslint.config.js
+++ eslint.config.js
@@ -18,7 +18,7 @@ export default tseslint.config(
                'react-refresh': reactRefresh,
           },
           rules: {
-               ...reactHooks.configs.recommended.rules,
+               // ...reactHooks.configs.recommended.rules,
                // 'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
                '@typescript-eslint/no-unused-vars': 'warn',
                '@typescript-eslint/no-unused-expressions': 'warn',
