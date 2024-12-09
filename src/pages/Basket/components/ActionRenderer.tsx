5eb815b773c7d14d57799b4f87369f810ed0af35
erfanGhayomi93 <erfuunghayomi@gmail.com>
2024-12-09 15:29:03 +0330

    fixed bug

--- src/pages/Basket/components/ActionRenderer.tsx
+++ src/pages/Basket/components/ActionRenderer.tsx
@@ -6,22 +6,30 @@ interface ActionRendererParams extends CustomCellRendererProps<IDetailsCartRes> 
      onDeleteOrder: (data: IDetailsCartRes | undefined) => void;
      onEditOrder: (data: IDetailsCartRes | undefined) => void;
      ordersLoading: boolean;
+     isLoadingDelete: boolean;
 }
 
-const ActionRenderer = ({ data, onSendOrder, onEditOrder, onDeleteOrder, ordersLoading }: ActionRendererParams) => {
+const ActionRenderer = ({ data, onSendOrder, onEditOrder, onDeleteOrder, ordersLoading, isLoadingDelete }: ActionRendererParams) => {
      return (
           <div className="flex h-full items-center justify-center gap-4">
                <button
-                    className="disabled:opacity-60"
+                    className="disabled:opacity-50"
                     onClick={() => onSendOrder(data)}
                     disabled={ordersLoading}
                >
                     <RocketIcon className="text-icon-default" />
                </button>
-               <button onClick={() => onEditOrder(data)}>
+               <button
+                    disabled={true}
+                    className='opacity-50'
+                    onClick={() => onEditOrder(data)}>
                     <DraftOutlineIcon className="text-icon-default" />
                </button>
-               <button onClick={() => onDeleteOrder(data)}>
+               <button
+                    onClick={() => onDeleteOrder(data)}
+                    disabled={isLoadingDelete}
+                    className="disabled:opacity-50"
+               >
                     <DeleteOutlineIcon className="text-icon-default" />
                </button>
           </div>
